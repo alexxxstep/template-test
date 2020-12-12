19e4da7f56e82d18447f6c22d43efc3afa11caec
alexxxstep <astep2004@gmail.com>
2020-12-12 22:31:36 +0200

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('');
+// Get Quote From API
+async function getQuote() {
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+  const apiUrl =
+    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+
+  try {
+    const res = await fetch(proxyUrl + apiUrl)
+    const data = await res.json()
+
+    console.log(data)
+  } catch (err) {
+    // getQuote()
+    console.log(JSON.stringify(err))
+  }
+}
+
+// On Load
+getQuote()
